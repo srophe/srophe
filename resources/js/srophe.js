1c50ab404936c5c73c0ea8ef525a4497c59f680e
Winona Salesky <wsalesky@gmail.com>
2018-12-10 10:55:13 -0500

    Remove cruft

--- resources/js/srophe.js
+++ resources/js/srophe.js
@@ -24,20 +24,6 @@ $.validator.setDefaults({
 	}
 });
 
-/*
- * 			
- * function validateMyForm() {
-		// The field is empty, submit the form.
-		if(!document.getElementById("honeypot").value) { 
-			return true;
-		} 
-		 // the field has a value it's a spam bot
-		else {
-			return false;
-		}
-	}
- * 
- */
 $("#email").validate({
         submitHandler: function(form) {
             if($('input#url').val().length == 0)
